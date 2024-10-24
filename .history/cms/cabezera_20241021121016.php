ded0c26971fac25b3498e51ca9914def2cbc098c
diego <diego@clase.es>
2024-10-24 12:03:08 +0200

    estilos

---
+++ .history/cms/cabezera_20241021121016.php
@@ -0,0 +1,26 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">CMS Panel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artículos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ajustes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cerrar Sesión</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ .history/cms/parts/sidebar_20241021121457.php
@@ -0,0 +1,9 @@
+
+<div class="col-md-3">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+                    <a href="#" class="list-group-item list-group-item-action">Artículos</a>
+                    <a href="#" class="list-group-item list-group-item-action">Usuarios</a>
+                    <a href="#" class="list-group-item list-group-item-action">Ajustes</a>
+                </div>
+            </div>
